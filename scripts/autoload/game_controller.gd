182f7f24ae2b67ac54f2800716770cb38e703957
Jakub Stankiewicz <jakub.stankiewicz@opoczta.pl>
2025-06-04 19:50:51 +0200

    obsluga 2 kontrolerow naraz

--- scripts/autoload/game_controller.gd
+++ scripts/autoload/game_controller.gd
@@ -5,26 +5,36 @@ signal lives_updated(player_id: int, lives: int)
 var max_lives := 10
 var players: Dictionary
 
+var player_devices = {}
+
+func _input(event):
+	if event is InputEventJoypadButton or event is InputEventJoypadMotion: #and event.pressed:
+		if event.device not in player_devices:
+			var new_player_id = player_devices.size() + 1
+			player_devices[event.device] = new_player_id
+			# print("Assigned controller ", event.device, " to player ", new_player_id)
+	
+
 func new_game(number_of_players):
-    for i in range(number_of_players):
-        add_player(i)
+	for i in range(number_of_players):
+		add_player(i)
 
 
 
 func add_player(player_id: int):
-    players[player_id] = max_lives
-    emit_signal("lives_updated", player_id, max_lives)
+	players[player_id] = max_lives
+	emit_signal("lives_updated", player_id, max_lives)
 
 
 
 func update_lives(player_id: int, amount: int):
-    if players.has(player_id):
-        players[player_id] += amount
-        emit_signal("lives_updated", player_id, players[player_id])
+	if players.has(player_id):
+		players[player_id] += amount
+		emit_signal("lives_updated", player_id, players[player_id])
 
-        if players[player_id] <= 0:
-            player_died(player_id)
+		if players[player_id] <= 0:
+			player_died(player_id)
 
 
 func player_died(player_id: int):
-    print("Player %d is out!" % player_id)
+	print("Player %d is out!" % player_id)
--- scripts/character.gd
+++ scripts/character.gd
@@ -12,18 +12,14 @@ var max_jump_count = 2
 var jump_count = 0
 var direction = 1.0
 
+@export var max_ammo: int = 10
+var current_ammo: int = 5
 
+var player_devices = GameController.player_devices
 
-#func shoot(shooting_direction):
-	#var projectile = projectile_scene.instantiate()
-	#
-	#if shooting_direction == -1.0:
-		#projectile.direction = Vector2.LEFT
-	#else:
-		#projectile.direction = Vector2.RIGHT
-#
-	#projectile.global_position = global_position + (projectile.direction * 60)#(offset)
-	#get_tree().current_scene.add_child(projectile)
+var previous_jump_pressed = false
+var previous_shoot_pressed = false
+
 
 func shoot(shooting_direction):
 	if current_ammo <= 0:
@@ -52,62 +48,63 @@ func jump():
 	jump_count += 1
 	velocity.y = jump_velocity
 
-@export var max_ammo: int = 10
-var current_ammo: int = 5
 
-
-func _physics_process(delta: float) -> void:
-	#label.text = str(jump_count)
+func _process(delta: float) -> void:
 	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-
-	if Input.get_axis("ui_left", "ui_right"):
-		direction = 1.0 if Input.get_axis("ui_left", "ui_right") > 0 else -1.0   # checking becuase gmepad joys give values other than 1.0 and -1.0
-		velocity.x = direction * speed
-		print(direction)
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
+		jump_count = 0
+
+	for device_id in player_devices.keys():
+		if player_devices[device_id] != player_id:
+			continue
+
+		var axis_x = Input.get_joy_axis(device_id, JOY_AXIS_LEFT_X)
+		direction = 1.0 if axis_x > 0 else -1.0 if axis_x < 0 else direction
+		velocity.x = direction * speed if abs(axis_x) > 0.1 else move_toward(velocity.x, 0, speed)
+
+		# Jump button edge detection
+		var jump_pressed = Input.is_joy_button_pressed(device_id, JOY_BUTTON_B)
+
+		if jump_pressed and not previous_jump_pressed:
+			if jump_count < max_jump_count:
+				jump()
+		previous_jump_pressed = jump_pressed
+		
+		# Shoot button edge detection
+		var shoot_pressed = Input.is_joy_button_pressed(device_id, JOY_BUTTON_A)
+
+		if shoot_pressed and not previous_shoot_pressed:
+			shoot(direction)
+		previous_shoot_pressed = shoot_pressed
 
 
-	## SHOOTING
-	if Input.is_action_just_pressed("shoot"):
-		shoot(direction)
-	
-	## JUMPING
-	if Input.is_action_just_pressed("jump") and (is_on_floor() or jump_count < max_jump_count):
-		jump()
-	
-
-	## Resetting the jump count
-	if is_on_floor():
-		jump_count = 1
 
 	move_and_slide()
 
-#HEALTH
-var max_health: int = 5
-var current_health: int = 3
+# #HEALTH
+# var max_health: int = 5
+# var current_health: int = 3
 
 
-func add_health(amount: int):
-	current_health = min(current_health + amount, max_health)
-	print("HP:", current_health)
-	update_health_label()
+# func add_health(amount: int):
+# 	current_health = min(current_health + amount, max_health)
+# 	print("HP:", current_health)
+# 	update_health_label()
 	
-func add_ammo(amount: int):
-	current_ammo = min(current_ammo + amount, max_ammo)
-	update_ammo_label()
+# func add_ammo(amount: int):
+# 	current_ammo = min(current_ammo + amount, max_ammo)
+# 	update_ammo_label()
 
 	
-func update_health_label():
-	var label_name = "player_%d_lives" % player_id
-	var label_path = "Arena/" + label_name
-	var label = get_tree().root.get_node(label_path)
-	label.text = "%d / %d" % [current_health, max_health]
+# func update_health_label():
+# 	var label_name = "player_%d_lives" % player_id
+# 	var label_path = "Arena/" + label_name
+# 	var label = get_tree().root.get_node(label_path)
+# 	label.text = "%d / %d" % [current_health, max_health]
 
-func _ready():
-	update_health_label()
-	update_ammo_label()
+# func _ready():
+# 	update_health_label()
+# 	update_ammo_label()
